709902f2c4732f272384ebc42486b066e747ac56
djones <dxjones00@yahoo.com>
2017-05-03 16:55:32 -0600

    170503

--- go1.py
+++ go1.py
@@ -3,6 +3,9 @@ import netsnmp
 from datetime import datetime
 
 
+w=5
+h=40
+matrix = [[0 for x in range(w)] for y in range(h)]
 
 f=open('testresult', 'a')
 
@@ -19,13 +22,40 @@ comm_string="public01"
 session=netsnmp.Session(Version=2, Community=comm_string, DestHost=host_ipa, UseNumeric=1)
 
 # pull MAC addresses of registered modems
-oid=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.2.1.10.127.1.3.3.1.2.1"))
+oid=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.2.1.10.127.1.3.3.1.2"))
 result=session.walk(oid)
 print result
+
+
+
 
 s=str(result)
 f.write(s)
 f.write("\n")
+
+# pull MACs from DOCS-IF3-MIB
+oid=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.4.1.4491.2.1.20.1.3.1.2"))
+result=session.walk(oid)
+print result
+
+f.write("MAC addresses from DOCS-IF3-MIB\n")
+s=str(result)
+f.write(s)
+f.write("\n")
+
+
+# get modem registration status (CMTS view)
+oid=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.4.4491.2.1.20.1.3.1.6"))
+regstat=session.walk(oid)
+print("Registration Status Values\n")
+print regstat
+
+f.write("Registration Status")
+s=str(regstat)
+f.write(s)
+f.write("\n")
+
+
 
 # get total codewords from RFC4546
 oid1=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.2.1.10.127.1.3.3.1.10"))
@@ -37,11 +67,21 @@ s=str(result)
 f.write(s)
 f.write("\n")
 
+matrix[1][:]=result
+
 
 # gets corrected codewords from RFC4546
 oid2=netsnmp.VarList(netsnmp.Varbind(".1.3.6.1.2.1.10.127.1.3.3.1.11"))
 result2=session.walk(oid2)
 print result2
+
+matrix[2][:]=result2
+
+print ("this is the matrix value\n")
+print matrix[1][0]
+print matrix[2][0]
+print ("\n")
+
 
 f.write("corrected codewords\n")
 s=str(result2)
